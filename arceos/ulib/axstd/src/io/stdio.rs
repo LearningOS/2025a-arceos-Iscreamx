ea13702cd0755f6c30e77bbf94600a628f5957b4
iscreamx <iscreamx404@gmail.com>
2025-11-04 16:39:29 +0800

    add color to print

--- arceos/ulib/axstd/src/io/stdio.rs
+++ arceos/ulib/axstd/src/io/stdio.rs
@@ -166,8 +166,12 @@ pub fn __print_impl(args: core::fmt::Arguments) {
     if cfg!(feature = "smp") {
         // synchronize using the lock in axlog, to avoid interleaving
         // with kernel logs
-        arceos_api::stdio::ax_console_write_fmt(args).unwrap();
+        arceos_api::stdio::ax_console_write_fmt(
+            format_args!("\x1b[32m{}\x1b[0m", args)
+        ).unwrap();
     } else {
-        stdout().lock().write_fmt(args).unwrap();
+        stdout().lock().write_fmt(
+            format_args!("\x1b[32m{}\x1b[0m", args)
+        ).unwrap();
     }
 }
