178a4c3c9554d45641aa9ab9496c6a0e81b38da6
iscreamx <iscreamx404@gmail.com>
2025-11-11 18:19:20 +0800

    support bump_allocator

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
-use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
+use allocator::{AllocError, AllocResult, BaseAllocator, ByteAllocator, PageAllocator};
+use core::ptr::NonNull;
 
 /// Early memory allocator
 /// Use it before formal bytes-allocator and pages-allocator can work!
@@ -16,21 +17,37 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    start: usize,
+    end:   usize,
+    b_pos: usize,
+    p_pos: usize,
+    b_count: usize,
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            end: 0,
+            b_pos: 0,
+            p_pos: 0,
+            b_count: 0,
+        }
     }
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.start = start;
+        self.end = start + size;
+        self.b_pos = start;
+        self.p_pos = start + size;
+        self.b_count = 0;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
-        todo!()
+        Err(AllocError::NoMemory)
     }
 }
 
@@ -39,23 +56,38 @@ impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        let size = layout.size();
+        let align = layout.align();
+        let aligned_b_pos = (self.b_pos + align - 1) & !(align - 1);
+        let new_b_pos = aligned_b_pos + size;
+        if new_b_pos > self.p_pos {
+            return Err(allocator::AllocError::NoMemory);
+        }
+        self.b_pos = new_b_pos;
+        self.b_count += 1;
+
+        NonNull::new(aligned_b_pos as *mut u8).ok_or(AllocError::InvalidParam)
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        if self.b_count > 0 {
+            self.b_count -= 1;
+            if self.b_count == 0 {
+                self.b_pos = self.start;
+            }
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!()
+        self.end - self.start
     }
 
     fn used_bytes(&self) -> usize {
-        todo!()
+        self.b_pos - self.start
     }
 
     fn available_bytes(&self) -> usize {
-        todo!()
+        self.end - self.b_pos
     }
 }
 
@@ -67,22 +99,32 @@ impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
         num_pages: usize,
         align_pow2: usize,
     ) -> allocator::AllocResult<usize> {
-        todo!()
+        let alloc_size = num_pages * SIZE;
+        let align = SIZE * (1 << align_pow2);
+        
+        let new_p_pos = (self.p_pos - alloc_size) & !(align - 1);
+        
+        if new_p_pos < self.b_pos {
+            return Err(AllocError::NoMemory);
+        }
+        
+        self.p_pos = new_p_pos;
+        Ok(new_p_pos)
     }
 
     fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
-        todo!()
+        // EarlyAllocator does not support deallocation of pages.
     }
 
     fn total_pages(&self) -> usize {
-        todo!()
+        (self.end - self.start) / SIZE
     }
 
     fn used_pages(&self) -> usize {
-        todo!()
+        (self.p_pos - self.start) / SIZE
     }
 
     fn available_pages(&self) -> usize {
-        todo!()
+        (self.end - self.p_pos) / SIZE
     }
 }
